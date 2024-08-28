18423e1cc9eb3664aa40e49ef80ddd59c117b30c
Nana Gaisie <nabgaisie@outlook.com>
2024-08-28 03:12:16 +0000

    Refactor login page to fix typo in "Don't have an account?"

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,7 +7,7 @@ function LoginPage() {
     <main className="flex h-full flex-col items-center justify-center">
       <SignInCard />
       <div className="flex items-center justify-center">
-        <p className="mr-2">Don't have an account?</p>
+        <p className="mr-2">Don&apos;t have an account?</p>
         <Link
           className={buttonVariants({
             variant: "link",
