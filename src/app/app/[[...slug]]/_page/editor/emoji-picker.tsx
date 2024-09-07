5d9f13c045bbd1160b51e051dd728fea4edb4ecd
Nana Gaisie <nabgaisie@outlook.com>
2024-09-07 23:06:17 +0000

    Refactor emoji-picker component to add theme support and handle outside click

--- src/app/app/[[...slug]]/_page/editor/emoji-picker.tsx
+++ src/app/app/[[...slug]]/_page/editor/emoji-picker.tsx
@@ -2,7 +2,33 @@
 
 import EmojiPicker from "emoji-picker-react";
 import { EmojiStyle, Theme } from "emoji-picker-react";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+function useOutsideClick(
+  callback: () => void,
+  excludeRef: React.RefObject<HTMLElement>,
+) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        ref.current &&
+        !ref.current.contains(event.target as Node) &&
+        !excludeRef.current?.contains(event.target as Node)
+      ) {
+        callback();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [callback, excludeRef]);
+
+  return ref;
+}
 
 function EmojiPickerComponent({
   children,
@@ -12,11 +38,22 @@ function EmojiPickerComponent({
   setEmojiIcon: (emoji: string) => void;
 }) {
   const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
+  const childrenRef = useRef<HTMLDivElement>(null);
+  const emojiPickerRef = useOutsideClick(
+    () => setOpenEmojiPicker(false),
+    childrenRef,
+  );
+
   return (
-    <div>
-      <div onClick={() => setOpenEmojiPicker(true)}>{children}</div>
+    <>
+      <div
+        ref={childrenRef}
+        onClick={() => setOpenEmojiPicker((prev) => !prev)}
+      >
+        {children}
+      </div>
       {openEmojiPicker && (
-        <div className="absolute z-10">
+        <div className="absolute z-10" ref={emojiPickerRef}>
           <EmojiPicker
             emojiStyle={EmojiStyle.NATIVE}
             theme={Theme.AUTO}
@@ -27,7 +64,7 @@ function EmojiPickerComponent({
           />
         </div>
       )}
-    </div>
+    </>
   );
 }
 
